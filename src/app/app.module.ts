b3170d7e2e83c70a2b2206f1a10e38e6c98b4aac
Tomas Garcia <tomasotano25@gmail.com>
2018-01-14 19:28:57 -0400

    logrado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+grdhdr
 
 @NgModule({
   declarations: [

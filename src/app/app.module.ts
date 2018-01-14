b3beeecc53b7db77dac0305dc1dde785af609e7e
Tomas Garcia <tomasotano25@gmail.com>
2018-01-14 19:17:05 -0400

    primera actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-import { TaskComponent } from './task/task.component';
-
+import { TasksComponent } from './components/tasks/tasks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskComponent
+    TasksComponent
   ],
   imports: [
     BrowserModule

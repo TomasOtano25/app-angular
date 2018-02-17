4e9efa803a4619d0c4463f906a06245e2dab94cb
Tomas Garcia <tomasotano25@gmail.com>
2018-02-17 16:23:14 -0400

    Trabajando en el login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,18 +9,31 @@ import { AuthService } from '../../services/auth.service';
 })
 export class LoginComponent implements OnInit {
 
+  email: string;
+  password: string;
+
   authState: any;
 
   constructor(public authService: AuthService) { }
 
   ngOnInit() {
-   this.authState =  this.authService.afAuth.authState;
+   this.authState =  this.authService.authState();
+  }
+
+  loginGoogle() {
+    this.authService.loginGoogle();
+  }
+
+  loginFacebook() {
+    this.authService.loginFacebook();
   }
 
   login() {
-    this.authService.login();
+    console.log(this.email, this.password);
+    this.authService.signon(this.email, this.password);
   }
 
+  // this.authService.login(this.email, this.password);
   logout() {
     this.authService.logout();
   }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,23 +1,63 @@
 import { Injectable } from '@angular/core';
 
 /* */
-import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuth, AngularFireAuthProvider } from 'angularfire2/auth';
 
 import * as firebase from 'firebase/app';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 
 
 @Injectable()
 export class AuthService {
 
-  constructor(public afAuth: AngularFireAuth) {
+  /*private user: Observable<firebase.User>;*/
+
+  constructor(private afAuth: AngularFireAuth, private af: AngularFirestore) {
   }
 
-  login() {
-    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+  loginGoogle() {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    // Me permite tener acceso a los contactos de los clientes
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  loginFacebook() {
+    const provider = new firebase.auth.FacebookAuthProvider();
+      this.afAuth.auth.signInWithPopup(provider);
+  }
+
+  /*loginTwitter() {
+    const provider = new firebase.auth.TwitterAuthProvider();
+      this.afAuth.auth.signInWithPopup(provider);
+  }*/
+
+  signon(email: string, password: string) {
+    this.afAuth.auth.signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error => {
+        console.log(`${error.code} ${error.message}`);
+      }));
+  }
+
+
+  signup(email: string, password: string) {
+    this.afAuth.auth.createUserWithEmailAndPassword(email, password)
+    .catch((error) => {
+      console.log(`${error.code} ${error.message}`);
+    });
   }
 
   logout() {
     this.afAuth.auth.signOut();
   }
+
+  authState(): any {
+    return this.afAuth.authState;
+  }
+
 }
